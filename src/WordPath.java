26d28b3a46e28cf343658bd76348bc3fe2bba229
ntbanks <ntbanks@unm.edu>
2017-01-19 14:23:47 -0700

    Added file i/o and words file

--- src/WordPath.java
+++ src/WordPath.java
@@ -1,8 +1,22 @@
 /**
  * Created by Nathan on 1/19/2017.
  */
+
+import java.io.*;
+
 public class WordPath {
     public static void main(String[] args){
-        System.out.println("hi");
+
+        try {
+            FileReader fr = new FileReader(new File("OpenEnglishWordList.txt"));
+            BufferedReader br = new BufferedReader(fr);
+            String word = null;
+            while ((word = br.readLine()) != null){
+                System.out.println(word);
+            }
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
